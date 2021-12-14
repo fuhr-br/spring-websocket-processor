1444e01054108d0d3532ab740312ed4e9e4d500f
fuhr-br <andersonfuhr@yahoo.com.br>
2021-12-14 17:00:00 -0300

    add Origin

--- src/main/java/tatsumibruno/samples/websocketprocessor/process/infrastructure/WebSocketConfiguration.java
+++ src/main/java/tatsumibruno/samples/websocketprocessor/process/infrastructure/WebSocketConfiguration.java
@@ -13,7 +13,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("https://clever-pasteur-5adacd.netlify.app")
                 .withSockJS();
     }
 
